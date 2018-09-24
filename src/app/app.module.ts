9bbf9c36b68ca50f404202b2cd16588410d002df
User <jeetrebel@gmail.com>
2018-09-25 00:41:25 +0530

    Templates files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes,RouterModule} from '@angular/router'
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+const appRoutes: Routes = [
+  { path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path:'home',
+    component:HomeComponent,
+    data: { title: 'Home' }
+  },
+  {
+    path:'about',
+    component:AboutComponent,
+    data: { title: 'About' }
+  },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
